dab9a1f659f89be41fc7c343d991db0525203b01
tessdja <tessdj@gmail.com>
2026-01-16 15:12:06 -0500

    experiments.ipynb updated with langchain practice and basic RAG commands

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mydoc_portal",
+    name="document_portal",
     author="Tess Aquino - from LLMOPS Live",
     version="0.1",
     packages=find_packages(),
