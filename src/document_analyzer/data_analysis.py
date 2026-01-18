e98b8bca23814b736eeee0a62365625c0328385d
tessdja <tessdj@gmail.com>
2026-01-17 19:01:06 -0500

    document analysis tested and is working using test.py

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -5,7 +5,7 @@ from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
-from langchain.output_parsers import OutputFixingParser
+from langchain_classic.output_parsers import OutputFixingParser
 from prompt.prompt_library import *
 
 class DocumentAnalyzer:
--- utils/model_loader.py
+++ utils/model_loader.py
@@ -60,7 +60,7 @@ class ModelLoader:
 
         log.info("Loading LLM...")
         
-        provider_key = os.getenv("LLM_PROVIDER", "groq")  # Default groq
+        provider_key = os.getenv("LLM_PROVIDER", "google")  # Default groq exceeds rate limits
         if provider_key not in llm_block:
             log.error("LLM provider not found in config", provider_key=provider_key)
             raise ValueError(f"Provider '{provider_key}' not found in config")
